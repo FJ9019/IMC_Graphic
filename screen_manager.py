f62d6bf4f056bd45fa778424bc0eabdc8346ae1e
FJ9019 <fenelus24@gmail.comgit config --global user.email fenelus24@gmail.comgit config --global user.email fenelus24@gmail.com>
2025-05-15 11:12:17 -0400

    Login Page Finish

--- screen_manager.py
+++ screen_manager.py
@@ -1,4 +1,11 @@
 from kivy.uix.screenmanager import ScreenManager
+from package.Data import Data
 
 class MyScreenManager(ScreenManager):
+    def check_credentials(self, pseudo:str, password:str):
+        print(f"{pseudo} : {password}")
+        get_user = Data.get_user(pseudo, password)
+        print(get_user)
+        if get_user['status']:
+            self.current = "index"
     pass
