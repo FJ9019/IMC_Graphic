37e976127c36a92c34adfc2d882dae208a339f89
FJ9019 <fenelus24@gmail.comgit config --global user.email fenelus24@gmail.comgit config --global user.email fenelus24@gmail.com>
2025-05-15 12:03:38 -0400

    Create Account Update

--- screen_manager.py
+++ screen_manager.py
@@ -2,10 +2,21 @@ from kivy.uix.screenmanager import ScreenManager
 from package.Data import Data
 
 class MyScreenManager(ScreenManager):
+    def get_gender(self, sex):
+        if sex == "Homme":
+            return 0
+        elif sex == "Femme":
+            return 1
+        
     def check_credentials(self, pseudo:str, password:str):
         print(f"{pseudo} : {password}")
         get_user = Data.get_user(pseudo, password)
         print(get_user)
         if get_user['status']:
             self.current = "index"
+    def check_signup(self, pseudo, password:str, password_repeat, nom_prenom, age, sex, taille, poidst, travail):
+
+        if password != "" and password == password_repeat:
+            user = user(pseudo, password, nom_prenom, age, sex, taille, poidst, travail)
+        pass
     pass
