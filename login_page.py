68914a8848cf06c81d448964bfb5ba0a4b657c4d
FJ9019 <fenelus24@gmail.comgit config --global user.email fenelus24@gmail.comgit config --global user.email fenelus24@gmail.com>
2025-08-13 16:05:56 -0400

    Update Footer

--- login_page.py
+++ login_page.py
@@ -1,8 +1,21 @@
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
+from kivy.properties import StringProperty
+from kivy.uix.label import Label
+
+from package.session import Session
 
 Builder.load_file("login_page.kv")
 
 
 class LoginPage(BoxLayout):
+    msg = StringProperty()
+    def check_session(self, widget: Label):
+        session = Session.get_session()
+        status = session['status']
+
+        if not status:
+            widget.text = "Utilisateur non trouvé"
+            widget.opacity = 1
+        pass
     pass
